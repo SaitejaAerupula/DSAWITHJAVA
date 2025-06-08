639c40507a865aed765c63e969f5543fd65cd02c
Saiteja Aerupula <144018477+SaitejaAerupula@users.noreply.github.com>
2025-06-08 19:36:46 +0530

    Add files via upload

---
+++ reversestr.java
@@ -0,0 +1,10 @@
+package java_program;
+
+public class add {
+	public static void main(String args[]) {
+		String Original = "Saiteja";
+		String reversed = new StringBuilder(Original).reverse().toString();
+		System.out.println("R"+reversed);
+	}
+
+}
