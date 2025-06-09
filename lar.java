4edccad98f155d309d4c9715c801d771a2d72d93
Saiteja Aerupula <144018477+SaitejaAerupula@users.noreply.github.com>
2025-06-09 08:13:11 +0530

    Add files via upload

---
+++ lar.java
@@ -0,0 +1,14 @@
+class lar {
+    public static void main(String[] args){
+        int a[]={1,4,2,3,9};
+        int max=a[0];
+        for(int i=1;i<a.length;i++){
+            if(a[i]>max){
+                max=a[i];
+            }
+        } System.out.print(max);
+    
+    
+}
+    
+}
